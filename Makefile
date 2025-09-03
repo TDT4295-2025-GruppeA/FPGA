7c2508ca88fcf64e6dbe85cfe4f3324356b58b71
Nicolai Sommervold <nicolai@sommervold.net>
2025-09-03 13:40:37 +0200

    feat: move logs to build folder

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
+
+BUILD_TIME := $(shell date +"%Y%m%d%H%M%S")
+
 synth:
-	vivado -mode batch -source scripts/synth.tcl
+	mkdir -p build/logs
+	vivado -mode batch -source scripts/synth.tcl -journal "build/logs/$(BUILD_TIME).jou"  -log "build/logs/$(BUILD_TIME).log"
 
 flash:
-	vivado -mode batch -source scripts/flash.tcl
+	mkdir -p build/logs
+	vivado -mode batch -source scripts/flash.tcl -journal "build/logs/$(BUILD_TIME).jou"  -log "build/logs/$(BUILD_TIME).log"
+
+clean:
+	rm -r build
+
+rmlogs:
+	rm -r build/logs
--- scripts/synth.tcl
+++ scripts/synth.tcl
@@ -2,6 +2,8 @@ package require fileutil
 
 set_param general.maxThreads 8
 
+set rpt_dir "build/reports"
+
 # Load configuration and code
 read_verilog [ fileutil::findByPattern src *.*v ]
 
@@ -15,23 +17,25 @@ read_verilog [ fileutil::findByPattern src *.*v ]
 # read_xdc verilog/vivado/constraints/nexysa7_cpu.xdc
 # synth_design -top Top -part xc7a100tcsg324-1
 
+# Arty A7
 read_xdc constraints/arty7.xdc
 synth_design -top Top -part xc7a35ticsg324-1L
 
-write_checkpoint -force report/post_synth_checkpoint
+write_checkpoint -force $rpt_dir/post_synth_checkpoint
 
 # Implement
 opt_design
 place_design
 route_design
 
-write_checkpoint -force report/post_route_checkpoint
+write_checkpoint -force $rpt_dir/post_route_checkpoint
 
 # Generate post-routing reports
-report_power -file report/post_route_power.rpt
-report_utilization -file report/post_route_utilization.rpt
-report_timing -delay_type min_max -max_paths 1 -file report/post_route_timing.rpt
+report_power -file $rpt_dir/post_route_power.rpt
+report_utilization -file $rpt_dir/post_route_utilization.rpt
+report_timing -delay_type min_max -max_paths 1 -file $rpt_dir/post_route_timing.rpt
 
 
 # Write bitstream result
+exec mkdir -p build
 write_bitstream -force build/top.bit
