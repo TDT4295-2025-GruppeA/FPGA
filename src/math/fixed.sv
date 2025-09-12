c49e84be8b290330e4cadd4ccf974d6eeea83ba7
Paulius Juzenas <pauliusj@stud.ntnu.no>
2025-09-12 09:32:37 +0200

    fix: add casting to fixed macros

--- src/math/fixed.sv
+++ src/math/fixed.sv
@@ -1,17 +1,22 @@
-localparam DECIMAL_WIDTH = 16;
-localparam TOTAL_WIDTH = 32;
+localparam DECIMAL_WIDTH = 24;
+localparam TOTAL_WIDTH = 64;
 
 typedef logic signed [TOTAL_WIDTH-1:0] fixed;
 
 // FADD and FSUB are not realy necessary,
 // but we define them for consistency.
 // It is also easier to change the implementation later if needed.
-`define FADD(l, r) (l + r)
-`define FSUB(l, r) (l - r)
-`define FMUL(l, r) ((`TOTAL_WIDTH*2)'(l) * (`TOTAL_WIDTH*2)'(r)) >>> DECIMAL_WIDTH
-`define FDIV(l, r) ({l, {DECIMAL_WIDTH{1'b0}}} / r)
+`define ADD(l, r) ((l) + (r))
+`define SUB(l, r) ((l) - (r))
+`define MUL(l, r) (((TOTAL_WIDTH*2)'(l) * (TOTAL_WIDTH*2)'(r)) >>> DECIMAL_WIDTH)
+`define DIV(l, r) (signed'({l, {DECIMAL_WIDTH{1'b0}}}) / signed'(r))
 
-`define FTOI(f) (f >>> DECIMAL_WIDTH)
-`define ITOF(i) (i <<< DECIMAL_WIDTH)
+// Convert fixed to integer (floor)
+`define FIXED_TO_INTEGER(f) (int'(f) >>> DECIMAL_WIDTH)
+// Convert integer to fixed
+`define ITOF(i) (fixed'(i) <<< DECIMAL_WIDTH)
 
-`define F(f) (f * (1 << DECIMAL_WIDTH))
+// Convert real to fixed
+`define F(f) (fixed'(f * real'(1 << DECIMAL_WIDTH)))
+// Convert fixed to real
+`define R(f) ((f) / real'(1 << DECIMAL_WIDTH))
