577614842354f4480ee30fcf3d8f96af18226ce7
Nicolai Sommervold <nicolai@sommervold.net>
2025-09-12 11:54:23 +0200

    fix: specify bitwidths to satisfy verilator

--- src/display/display.sv
+++ src/display/display.sv
@@ -2,7 +2,6 @@
 // https://github.com/projf/projf-explore/blob/main/lib/clock/xc7/clock_480p.sv
 
 import video_modes_pkg::*;
-import upscale_img_pkg::*;
 
 module Display #(
     parameter video_mode_t VIDEO_MODE = VMODE_640x480p60,
@@ -20,39 +19,48 @@ module Display #(
 );
     // Generate pixel coords and hsync/vsync
     // TODO: this can probably be simplified.
-    localparam H_RESOLUTION = VIDEO_MODE.h_resolution - 1;
-    localparam H_FRONT_PORCH = VIDEO_MODE.h_front_porch;
-    localparam H_SYNC = VIDEO_MODE.h_sync;
-    localparam H_BACK_PORCH = VIDEO_MODE.h_back_porch;
-    localparam LINEWIDTH = H_RESOLUTION + H_FRONT_PORCH + H_SYNC + H_BACK_PORCH;
+    localparam int H_RESOLUTION = VIDEO_MODE.h_resolution;
+    localparam int H_FRONT_PORCH = VIDEO_MODE.h_front_porch;
+    localparam int H_SYNC = VIDEO_MODE.h_sync;
+    localparam int H_BACK_PORCH = VIDEO_MODE.h_back_porch;
+    localparam int LINEWIDTH = H_RESOLUTION + H_FRONT_PORCH + H_SYNC + H_BACK_PORCH;
 
-    localparam V_RESOLUTION = VIDEO_MODE.v_resolution - 1;
-    localparam V_FRONT_PORCH = VIDEO_MODE.v_front_porch;
-    localparam V_SYNC = VIDEO_MODE.v_sync;
-    localparam V_BACK_PORCH = VIDEO_MODE.v_back_porch;
-    localparam LINEHEIGHT = V_RESOLUTION + V_FRONT_PORCH + V_SYNC + V_BACK_PORCH;
+    localparam int V_RESOLUTION = VIDEO_MODE.v_resolution;
+    localparam int V_FRONT_PORCH = VIDEO_MODE.v_front_porch;
+    localparam int V_SYNC = VIDEO_MODE.v_sync;
+    localparam int V_BACK_PORCH = VIDEO_MODE.v_back_porch;
+    localparam int LINEHEIGHT = V_RESOLUTION + V_FRONT_PORCH + V_SYNC + V_BACK_PORCH;
 
-    logic [$clog2(LINEWIDTH):0] x;
-    logic [$clog2(LINEHEIGHT):0] y;
+
+    // These are intentionally 1 bit wider than resolution, due to
+    // front+back porch and sync window.
+    localparam int VW = $clog2(LINEWIDTH) + 1;
+    localparam int VH = $clog2(LINEHEIGHT) + 1;
+    logic [VW - 1:0] x;
+    logic [VH - 1:0] y;
 
     // Set hsync and vsync flags
     logic hsync, vsync;
     logic hsync_nopol, vsync_nopol;
     logic data_enable;
     always_comb begin
-        hsync_nopol = (x >= (H_RESOLUTION + H_FRONT_PORCH) && x < (H_RESOLUTION + H_FRONT_PORCH + H_SYNC));
-        vsync_nopol = (y >= (V_RESOLUTION + V_FRONT_PORCH) && y < (V_RESOLUTION + V_FRONT_PORCH + V_SYNC));
+        hsync_nopol = (x >= VW'(H_RESOLUTION-1 + H_FRONT_PORCH) 
+                    && x < VW'(H_RESOLUTION-1 + H_FRONT_PORCH + H_SYNC));
+        vsync_nopol = (y >= VH'(V_RESOLUTION-1 + V_FRONT_PORCH)
+                    && y < VH'(V_RESOLUTION-1 + V_FRONT_PORCH + V_SYNC));
+
         hsync = (VIDEO_MODE.h_sync_pol) ? hsync_nopol : ~hsync_nopol;
         vsync = (VIDEO_MODE.v_sync_pol) ? vsync_nopol : ~vsync_nopol;
-        data_enable = (x <= H_RESOLUTION && y <= V_RESOLUTION);
+        data_enable = (x <= VW'(H_RESOLUTION - 1)
+                    && y <= VH'(V_RESOLUTION - 1));
     end
 
 
     // Iterate through pixels in image
     always_ff @(posedge clk_pixel or negedge rstn_pixel) begin
-        if (x == LINEWIDTH) begin
+        if (x == VW'(LINEWIDTH-1)) begin
             x <= 0;
-            y <= (y == LINEHEIGHT) ? 0 : y + 1;
+            y <= (y == VH'(LINEHEIGHT-1)) ? 0 : y + 1;
         end else begin
             x <= x + 1;
         end
@@ -70,9 +78,14 @@ module Display #(
     // ##### Drawing from image #####
     // ##############################
 
-
+    // TODO: dynamic scaled pixel_addr
     logic[31:0] pixel_addr;
     logic[11:0] fb_data;
+    
+    // Only scales based on width, and assumes a multiple of 2.
+    localparam int SCALE = $clog2(H_RESOLUTION / BUFFER_WIDTH);
+    assign pixel_addr = (32'(y) >> 1) * BUFFER_WIDTH + (32'(x) >> 1);
+
     Buffer #(
         .FILE_SOURCE("static/red_320x240p12.mem"),
         .FILE_SIZE(BUFFER_WIDTH * BUFFER_HEIGHT)
@@ -82,11 +95,6 @@ module Display #(
         .addr(pixel_addr),
         .data(fb_data)
     );
-
-    always_ff @(posedge clk_pixel) begin
-        pixel_addr <= get_upscaled_address(x, y, BUFFER_WIDTH, BUFFER_HEIGHT, VIDEO_MODE);
-    end
-
 
     // Draw from image buffer
     logic [3:0] paint_r, paint_g, paint_b;
@@ -101,28 +109,8 @@ module Display #(
         display_b = (data_enable) ? paint_b : 4'h0;
     end
 
-
-    // ####################################
-    // ##### Drawing stuff with maths #####
-    // ####################################
-
-
-    // logic [3:0] paint_r, paint_g, paint_b;
-    // always_comb begin
-    //     paint_r =   x; // (square) ? 4'hF : 4'h0;
-    //     paint_g =   y; // (square) ? 4'hF : 4'h0;
-    //     paint_b = 4'hF; // (square) ? 4'hF : 4'h0;
-    // end
-
-    // // display colour: paint colour but black in blanking interval
-    // logic [3:0] display_r, display_g, display_b;
-    // always_comb begin
-    //     display_r = (data_enable) ? paint_r : 4'h0;
-    //     display_g = (data_enable) ? paint_g : 4'h0;
-    //     display_b = (data_enable) ? paint_b : 4'h0;
-    // end
-
-    // Flip-flopts for output
+    // Flip-flops for output
+    // TODO: remove/simplify
     logic hsync_delay;
     logic vsync_delay;
     logic hsync_delay2;
