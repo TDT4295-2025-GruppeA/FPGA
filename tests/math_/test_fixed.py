61f53a8ab8211ce33e365c4b9a9b96ccf48d8f76
Paulius Juzenas <pauliusj@stud.ntnu.no>
2025-09-15 18:03:53 +0200

    fx: better typing in test utils

--- tests/math_/test_fixed.py
+++ tests/math_/test_fixed.py
@@ -7,8 +7,6 @@ from utils import within_tolerance
 
 VERILOG_MODULE = "FixedTB"
 
-TOLERANCE = 0.001
-
 TEST_VALUES = set(np.linspace(-1, 1, 101) + np.linspace(-10, 10, 101))
 
 @cocotb.test()
@@ -20,11 +18,11 @@ async def test_fixed_arithmetic(dut: Fixedtb):
             dut.a.value = a
             dut.b.value = b
 
-            await Timer(10, units="ns")
+            await Timer(1)
 
             assert within_tolerance(dut.sum.value, a + b), f"Addition failed: {a:.5f} + {b:.5f} = {dut.sum.value:.5f} != {a + b:.5f}"
-            assert within_tolerance(dut.sub.value, a - b), f"Subtraction failed: {a:.5f} - {b:.5f} = {dut.diff.value:.5f} != {a - b:.5f}"
-            assert within_tolerance(dut.mul.value, a * b), f"Multiplication failed: {a:.5f} * {b:.5f} = {dut.prod.value:.5f} != {a * b:.5f}"
+            assert within_tolerance(dut.sub.value, a - b), f"Subtraction failed: {a:.5f} - {b:.5f} = {dut.sub.value:.5f} != {a - b:.5f}"
+            assert within_tolerance(dut.mul.value, a * b), f"Multiplication failed: {a:.5f} * {b:.5f} = {dut.mul.value:.5f} != {a * b:.5f}"
             if b != 0:
                 assert within_tolerance(dut.div.value, a / b), f"Division failed: {a:.5f} / {b:.5f} = {dut.div.value:.5f} != {a / b:.5f}"
     
--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
-from cocotb.handle import ArrayObject
-from cocotb.types import Array
+from cocotb.types import Array, LogicArray
 
 TOLERANCE = 0.001
 
@@ -11,7 +10,7 @@ def to_fixed(value):
     """Convert a float to fixed point."""
     res = (value * (1 << DECIMAL_WIDHT))
 
-    if isinstance(value, np.ndarray):
+    if isinstance(res, np.ndarray):
         return res.astype(int)
     
     return int(res)
@@ -20,22 +19,26 @@ def to_float(value):
     """Convert a fixed point to float."""
     return value / (1 << DECIMAL_WIDHT)
 
-def within_tolerance(a, b, tolerance = TOLERANCE):
+def within_tolerance(a: int | float | np.ndarray, b: int | float | np.ndarray, tolerance: float = TOLERANCE) -> bool:
     """Check if two values are within a certain tolerance. The values may be scalars or numpy arrays."""
-    return np.any(np.abs(a - b) < tolerance)
+    return bool(np.allclose(a, b, atol=TOLERANCE, rtol=0))
 
-def cocotb_to_numpy(cocotb_matrix: ArrayObject) -> np.ndarray:
+def cocotb_to_numpy(cocotb_matrix: Array) -> np.ndarray:
     """Converts a cocotb ArrayObject of fixed point integers to a numpy array of floats."""
-    def convert_element(elem):
+    def convert_element(elem) -> int | np.ndarray:
         if isinstance(elem, Array):
             return np.array([convert_element(e) for e in elem])
 
-        return int(elem)
+        if isinstance(elem, LogicArray):
+            return elem.to_signed()
+        
+        # This should never happen
+        raise RuntimeError("Element is not an Array or LogicArray")
     
     res = convert_element(cocotb_matrix)
 
-    return to_float(res)
+    return to_float(res) # type: ignore
 
 def numpy_to_cocotb(np_matrix: np.ndarray) -> list:
     """Converts a numpy array of floats to a cocotb ArrayObject of fixed point integers."""
-    return to_fixed(np_matrix).tolist()
+    return to_fixed(np_matrix).tolist() # type: ignore
